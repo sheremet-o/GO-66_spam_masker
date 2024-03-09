e3ba6d3ff414a6b234288613afa42e10cfd1fea2
Oksana Sheremet <sheremet5631@gmail.com>
2024-03-09 10:14:18 +0300

    added tests

--- GO_spam_masker_test.go
+++ GO_spam_masker_test.go
@@ -7,13 +7,15 @@ func TestMasker(t *testing.T) {
 		input    string
 		expected string
 	}{
-		{input: "Here's my spammy page: http://hehefouls.netHAHAHA see you.", expected: "Here's my spammy page: http://******************* see you."},
+		{"Here's my spammy page: http://hehefouls.netHAHAHA see you.", "Here's my spammy page: http://******************* see you."},
+		{"Перейдите по ссылке https://www.google.com для получения информации", "Перейдите по ссылке https://www.google.com для получения информации"},
+		{"http://test.com is a test website", "http://******** is a test website"},
 	}
 
 	for _, testCase := range testCases {
 		result := masker(testCase.input)
 		if result != testCase.expected {
-			t.Errorf("Должно быть %s", testCase.expected)
+			t.Errorf("Должно быть %s, а получаем %s", testCase.expected, result)
 		}
 	}
 }
