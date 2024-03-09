1c6315ffa5c777b6a7820be5d9e5e245812dc8fe
Oksana Sheremet <sheremet5631@gmail.com>
2024-03-09 10:50:11 +0300

    Исправления после ревью-1

--- GO_spam_masker.go
+++ GO_spam_masker.go
@@ -4,29 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sheremet-o/GO_spam_masker.git/masker"
 )
-
-func masker(message string) string {
-	buffer := []byte(message)
-	linkHttp := []byte("http://")
-
-	for i := 0; i < len(buffer)-len(linkHttp); i++ {
-		if string(buffer[i:i+len(linkHttp)]) == string(linkHttp) {
-			j := i + len(linkHttp)
-			for j < len(buffer) && buffer[j] != ' ' {
-				buffer[j] = '*'
-				j++
-			}
-			i = j
-		}
-	}
-	return string(buffer)
-}
 
 func main() {
 	message := bufio.NewReader(os.Stdin)
 	fmt.Print("Введите текст: ")
 	words, _ := message.ReadString('\n')
-	maskedMessage := masker(words)
+	maskedMessage := masker.Masker(words)
 	fmt.Println(maskedMessage)
 }
--- GO_spam_masker_test.go
+++ GO_spam_masker_test.go
@@ -1,6 +1,25 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
+
+func Masker(message string) string {
+	buffer := []byte(message)
+	linkHttp := []byte("http://")
+
+	for i := 0; i < len(buffer)-len(linkHttp); i++ {
+		if string(buffer[i:i+len(linkHttp)]) == string(linkHttp) {
+			j := i + len(linkHttp)
+			for j < len(buffer) && buffer[j] != ' ' {
+				buffer[j] = '*'
+				j++
+			}
+			i = j
+		}
+	}
+	return string(buffer)
+}
 
 func TestMasker(t *testing.T) {
 	testCases := []struct {
@@ -13,7 +32,7 @@ func TestMasker(t *testing.T) {
 	}
 
 	for _, testCase := range testCases {
-		result := masker(testCase.input)
+		result := Masker(testCase.input)
 		if result != testCase.expected {
 			t.Errorf("Должно быть %s, а получаем %s", testCase.expected, result)
 		}
