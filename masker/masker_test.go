e59cf9343f3bdc16708fa46b4a3efce188e1d2d5
Oksana Sheremet <sheremet5631@gmail.com>
2024-03-11 09:21:36 +0300

    moved file with tests to 'masker' directory

--- masker/masker_test.go
+++ masker/masker_test.go
@@ -1,25 +1,8 @@
-package main
+package masker
 
 import (
 	"testing"
 )
-
-func Masker(message string) string {
-	buffer := []byte(message)
-	linkHttp := []byte("http://")
-
-	for i := 0; i < len(buffer)-len(linkHttp); i++ {
-		if string(buffer[i:i+len(linkHttp)]) == string(linkHttp) {
-			j := i + len(linkHttp)
-			for j < len(buffer) && buffer[j] != ' ' {
-				buffer[j] = '*'
-				j++
-			}
-			i = j
-		}
-	}
-	return string(buffer)
-}
 
 func TestMasker(t *testing.T) {
 	testCases := []struct {
